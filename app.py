654f675577a152e52b8791aefa248fcd8f95f254
Vytautas <51360361+vytautasmatukynas@users.noreply.github.com>
2023-06-25 17:34:34 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ app.config['SECRET_KEY'] = "my secret key"
 # Import blueprints
 from views.main_table import main_table
 from views.edit_table import edit_table
+from error_handler.handlers import error_pages
 
 # Register blueprints
 app.register_blueprint(main_table)
 app.register_blueprint(edit_table)
+app.register_blueprint(error_pages)
 
 
 if __name__ == "__main__":
